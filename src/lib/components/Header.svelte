7bfa0c5c18a67558e9b8755cc02d2ba60cd3e562
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2025-02-20 00:53:30 +0200

    заголовок для перевірки

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+</script>
+
+<header class="flex py-4 px-4 bg-black text-white items-center justify-between relative z-50">
+    <div class="flex items-center space-x-4">
+        <span>Foo</span>
+    </div>
+
+    <div class="flex items-center space-x-4">
+        <span>Bar</span>
+        <span>Baz</span>
+    </div>
+</header>
