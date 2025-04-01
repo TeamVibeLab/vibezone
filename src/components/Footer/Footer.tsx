4190b64bf6dfc852363f470c7f1300c588b0f804
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2025-04-01 20:02:22 +0300

    незавершений футер

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,37 @@
+const footerWebsiteLinks = [
+    {
+        href: "/locations",
+        text: "Локації",
+    },
+    {
+        href: "/news",
+        text: "Оголошення",
+    },
+    {
+        href: "/about",
+        text: "Про нас",
+    },
+]
+
+const footerExternalLinks = [
+    {
+        href: "https://www.google.com/",
+        text: "TikTok",
+    },
+    {
+        href: "https://www.google.com/",
+        text: "Instagram",
+    },
+    {
+        href: "https://www.google.com/",
+        text: "Telegram",
+    },
+]
+
+export default function Footer() {
+    return (
+        <footer className="mx-2 my-3 border-4 border-white rounded-4xl" >
+            etsttestsetest
+        </footer>
+    )
+}
