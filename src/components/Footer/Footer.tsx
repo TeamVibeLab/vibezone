6f82f8206dd10b5f1e834ea8920d46300f4a890b
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2025-04-06 00:56:06 +0300

    можливе завершення футера

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -48,30 +48,45 @@ const contactLinks = [
 
 export default function Footer() {
     return (
-        <footer className="mx-2 my-3
+        <footer className="mx-2 my-3 pt-6 pb-3
         border-4 border-white rounded-4xl
-        grid grid-cols-4" >
-            <Image
-                src="/images/logo.png"
-                alt="Логотип VibeZone"
-                width={107}
-                height={17}
-            />
-            <nav className={`grid grid-rows-${footerWebsiteLinks.length}`}>
-                {footerWebsiteLinks.map(item => (
-                    <Link key={item.text} {...item}>{item.text}</Link>
-                ))}
-            </nav>
-            <nav className={`grid grid-rows-${footerExternalLinks.length}`}>
-                {footerExternalLinks.map(item => (
-                    <Link key={item.text} {...item}>{item.text}</Link>
-                ))}
-            </nav>
-            <nav className={`grid grid-rows-${contactLinks.length}`}>
-                {contactLinks.map(item => (
-                    <Link key={item.text} {...item}>{item.text}</Link>
-                ))}
-            </nav>
+        bg-black" >
+            <div className="px-100
+            flex flex-row items-start justify-center">
+                <Image
+                    src="/images/logo.png"
+                    alt="Логотип VibeZone"
+                    width={200}
+                    height={0}
+                    className="mx-auto px-4"
+                />
+                <NavLinks title="Сторінки" linkParams={footerWebsiteLinks}/>
+                <NavLinks title="Соціальні мережі" linkParams={footerExternalLinks}/>
+                <NavLinks title="Контакти" linkParams={contactLinks}/>
+            </div>
+            <p className="text-xs text-gray-500
+            pt-6
+            flex items-center justify-center">&copy;2025 VibeZone. Створено з турботою про вас ;&#41;</p>
         </footer>
+    )
+}
+
+type HyperlinkParams = {
+    href: string;
+    text: string;
+}
+
+function NavLinks({ title, linkParams } : { title: string, linkParams: Array<HyperlinkParams> })
+{
+    return (
+        <div className="max-w-7xl mx-auto px-4 flex flex-col">
+            <p className="text-xl font-bold">{title}</p>
+            <nav className={`grid grid-rows-${linkParams.length}
+            flex flex-col gap-0.5`}>
+                {linkParams.map(item => (
+                    <a className="text-base transition hover:opacity-75" key={item.text} {...item}>{item.text}</a>
+                ))}
+            </nav>
+        </div>
     )
 }
