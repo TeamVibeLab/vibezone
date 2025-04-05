e3bc1e34ed84b8c9bd8cf83773c98808756ec60f
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2025-04-05 20:41:49 +0300

    продовження праці над футером

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,10 @@
+import Link from "next/link";
+
 const footerWebsiteLinks = [
+    {
+        href: "/home",
+        text: "Головна",
+    },
     {
         href: "/locations",
         text: "Локації",
@@ -11,27 +17,38 @@ const footerWebsiteLinks = [
         href: "/about",
         text: "Про нас",
     },
-]
+];
 
 const footerExternalLinks = [
     {
-        href: "https://www.google.com/",
+        href: "https://www.tiktok.com/",
         text: "TikTok",
     },
     {
-        href: "https://www.google.com/",
+        href: "https://www.instagram.com/",
         text: "Instagram",
     },
     {
-        href: "https://www.google.com/",
+        href: "https://web.telegram.org/",
         text: "Telegram",
     },
-]
+];
 
 export default function Footer() {
     return (
-        <footer className="mx-2 my-3 border-4 border-white rounded-4xl" >
-            etsttestsetest
+        <footer className="mx-2 my-3
+        border-4 border-white rounded-4xl
+        grid grid-cols-4" >
+            <nav className="grid grid-rows-4">
+                {footerWebsiteLinks.map(item => (
+                    <Link key={item.text} {...item}>{item.text}</Link>
+                ))}
+            </nav>
+            <nav className="grid grid-rows-4">
+                {footerExternalLinks.map(item => (
+                    <Link key={item.text} {...item}>{item.text}</Link>
+                ))}
+            </nav>
         </footer>
     )
 }
