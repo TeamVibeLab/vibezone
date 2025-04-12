4947bdc26283c92cbea0de564e00adbeb8cd931a
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2025-04-12 15:18:04 +0300

    початок головної сторінки + покращення колонтитулів

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex flex-col h-screen">
         <Header/>
-        <main>{children}</main>
+        <main className="flex-grow">{children}</main>
         <Footer/>
       </body>
     </html>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -23,16 +23,16 @@ export default function Header() {
     return (
         <header className="z-100
         flex justify-center
-        pt-4 pb-4 w-full
-        fixed top-0
+        pt-4 pb-4 mb-3 w-full
+        sticky top-0 left-0 right-0
         rounded-b-4xl bg-bright-foreground">
             <div className="flex flex-row items-start justify-center gap-x-8
-            max-w-8xl">
+            max-w-7xl">
                 <LogoImage bright={false} extraClassName="px-4"/>
 
                 <nav className={`flex flex-row gap-x-4`}>
                     {headerWebsiteLinks.map(item => (
-                        <a className="text-3xl text-bold text-dark-foreground transition hover:font-extrabold" key={item.text} {...item}>{item.text}</a>
+                        <a className="text-3xl text-bold text-dark-foreground transition hover:bold-shadow-dark-foreground" key={item.text} {...item}>{item.text}</a>
                     ))}
                 </nav>
             </div>
