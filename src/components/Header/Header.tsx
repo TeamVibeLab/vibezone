452296ff88cfb8b4f321acd5eb02e036b9a1b5f3
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2025-04-15 13:20:25 +0300

    початок колонтитула, який відображається на малих екранах

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -29,12 +29,16 @@ export default function Header() {
     const [displayMenuSmall, setDisplayMenuSmall] = useState(false);
     
     return (
-        <header className="z-100
+        <header className={`z-100
         flex justify-center
         pt-4 pb-4 mb-3 w-full
-        sticky top-0 left-0 right-0
-        rounded-b-4xl bg-bright-foreground">
-            <div className="flex flex-row w-full items-start md:justify-center md:gap-x-8 max-md:justify-between my-auto">
+        md:sticky
+        ${displayMenuSmall ? `max-md:grow max-md:min-h-screen max-md:fixed
+            max-md:justify-start` : `max-md:sticky`}
+        top-0 left-0 right-0
+        rounded-b-4xl bg-bright-foreground`}>
+            <div className={`flex flex-row w-full items-start md:justify-center max-md:justify-between
+                md:gap-x-8`}>
                 <LogoImage bright={false} extraClassName="px-4"/>
 
                 <button className="p-2 mr-6 md:hidden hover:bg-dark-foreground/50 rounded-lg"
@@ -48,6 +52,11 @@ export default function Header() {
                     />
                 </button>
 
+                {displayMenuSmall && (
+                    <nav className="flex flex-col gap-y-4 md:hidden">
+
+                    </nav>
+                )}
                 <nav className="flex flex-row gap-x-4 max-md:hidden">
                     {headerWebsiteLinks.map(item => (
                         <a className="text-2xl text-bold text-dark-foreground hover:bold-shadow-dark-foreground" key={item.text} {...item}>{item.text}</a>
