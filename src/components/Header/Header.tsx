c42d99b3a01691ef5acbcf4612ec3286a8fad8af
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2025-04-23 12:51:24 +0300

    дрібні зміни щодо хедера

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -38,7 +38,7 @@ export default function Header() {
         top-0 left-0 right-0
         rounded-b-4xl bg-bright-foreground`}>
             <div className="flex flex-col w-full gap-y-2">
-                <div className="flex flex-row w-full items-start md:justify-center max-md:justify-between md:gap-x-8">
+                <div className="flex flex-row w-full items-center md:justify-center max-md:justify-between md:gap-x-8">
                     <LogoImage bright={false} extraClassName="mx-4"/>
 
                     <button className="p-2 mx-4 md:hidden hover:bg-dark-foreground/50 rounded-lg"
@@ -62,7 +62,7 @@ export default function Header() {
                     text-right
                     md:hidden ${!displayMenuSmall && `hidden`}`}>
                     {headerWebsiteLinks.map(item => (
-                        <a className="px-2 text-2xl text-dark-foreground rounded-lg hover:bg-dark-foreground/20 hover:bold-shadow-dark-foreground" key={item.text} {...item}>{item.text}</a>
+                        <a className="px-2 text-2xl text-dark-foreground rounded-lg bg-dark-foreground/20 hover:bg-dark-foreground/40 hover:bold-shadow-dark-foreground" key={item.text} {...item}>{item.text}</a>
                     ))}
                 </div>
             </div>
