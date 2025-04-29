2f2b2987fee22cd66d1eef897ff15f69fde582fe
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2025-04-30 01:50:06 +0300

    продовження головної сторінки

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 export default function Home() {
   return (
     <div>
-      <section className="flex gap-x-2">
-        <div>
+      <section className="flex flex-col gap-x-2
+      rounded-4xl bg-bright-foreground p-6">
+        <button className="rounded-full bg-button text-white hover:bg-button-highlight">Знайти цікаві місця</button>
+        <div className="text-dark-foreground">
         Твій студентський путівник у Львові! Знаходь найкращі місця для відпочинку, натхнення та розваг
         </div>
-        <button className="rounded-full bg-sky-500 text-white hover:bg-sky-700">Знайти цікаві місця</button>
       </section>
     </div>
   );
