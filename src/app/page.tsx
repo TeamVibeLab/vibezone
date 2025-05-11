9fba5c59be04c35da954c53bd0c6b9eb13810a0a
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2025-05-12 01:11:08 +0300

    продовження топ місць

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,36 @@ export default function Home() {
 
   const rows = [];
   for (let i = 0; i < 16; i++) {
-      // note: we are adding a key prop here to allow react to uniquely identify each
-      // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
-      rows.push(<li key={i} className="snap-center">test{i}</li>);
+      rows.push(
+        <div key={i} className="w-85 h-128
+        shrink-0
+        relative
+        snap-center">
+          <Image
+              src={`/images/example_location_image.jpg`}
+              alt="Фотографія локації"
+              width={5233}
+              height={3758}
+              className="border-6 rounded-4xl border-dark-foreground
+              w-full h-full
+              object-bottom object-cover"
+              key={i}
+          />
+          <h2 className="absolute bottom-5 right-5
+          text-4xl text-center font-bold text-red-500">Test</h2>
+        </div>
+        /*<Image
+              src={`/images/example_location_image.jpg`}
+              alt="Фотографія локації"
+              width={5233}
+              height={3758}
+              className="border-6 rounded-4xl border-dark-foreground
+              w-85 h-128 
+              object-bottom object-cover
+              snap-center"
+              key={i}
+          />*/
+    );
   }
 
   return (
@@ -35,8 +62,7 @@ export default function Home() {
           /></span>
         </button>
         <div className="lg:w-1/2 max-lg:w-full
-        text-4xl
-        font-bold text-dark-foreground">
+        text-4xl font-bold text-dark-foreground">
         Твій студентський путівник у Львові! Знаходь найкращі місця для відпочинку, натхнення та розваг
         </div>
       </section>
@@ -45,8 +71,8 @@ export default function Home() {
         w-1/2 mx-auto m-5
         rounded-4xl bg-bright-foreground
         text-dark-foreground">
-        <p>Топ місць цього тижня!</p>
-        <ul className="flex w-19/20 snap-x snap-mandatory gap-6 overflow-x-auto">
+        <p className="text-4xl">Топ місць цього тижня!</p>
+        <ul className="flex w-full snap-x snap-mandatory gap-6 overflow-x-auto">
           {rows}
         </ul>
       </section>
