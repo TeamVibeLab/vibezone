ffca85cfe8cac93b5b75c38c089295fd9ea2bcea
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2025-05-20 01:07:18 +0300

    початок сторінки новин

---
+++ src/app/news/page.tsx
@@ -0,0 +1,47 @@
+import NewsPost from "@/components/NewsPost/NewsPost"
+
+export default function News() {
+    return (
+        <section className="w-3/5 p-4
+        rounded-lg bg-bright-foreground/50">
+
+            <div className="flex flex-col gap-4">
+                <NewsPost
+                    title="Lorem Ipsum"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus nulla sit amet sollicitudin semper. "
+                    date="January 1st, 2000"
+                />
+                <NewsPost
+                    title="Lorem Ipsum"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus nulla sit amet sollicitudin semper. "
+                    date="January 1st, 2000"
+                />
+                <NewsPost
+                    title="Lorem Ipsum"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus nulla sit amet sollicitudin semper. "
+                    date="January 1st, 2000"
+                />
+                <NewsPost
+                    title="Lorem Ipsum"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus nulla sit amet sollicitudin semper. "
+                    date="January 1st, 2000"
+                />
+                <NewsPost
+                    title="Lorem Ipsum"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus nulla sit amet sollicitudin semper. "
+                    date="January 1st, 2000"
+                />
+                <NewsPost
+                    title="Lorem Ipsum"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus nulla sit amet sollicitudin semper. "
+                    date="January 1st, 2000"
+                />
+                <NewsPost
+                    title="Lorem Ipsum"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus nulla sit amet sollicitudin semper. "
+                    date="January 1st, 2000"
+                />
+            </div>
+        </section>
+    );
+}
